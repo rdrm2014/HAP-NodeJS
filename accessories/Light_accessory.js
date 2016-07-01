9e54c5579fbf4a77b855c3428ded6cbb06a012f1
rdrm2014 <ricardomendes@staff-3081.wireless.ua.pt>
2016-07-01 15:34:52 +0100

    Change Authentication

--- accessories/Light_accessory.js
+++ accessories/Light_accessory.js
@@ -27,7 +27,7 @@ var FAKE_LIGHT = {
 var lightUUID = uuid.generate('hap-nodejs:accessories:light');
 
 // This is the Accessory that we'll return to HAP-NodeJS that represents our fake light.
-var light = exports.accessory = new Accessory('Light', lightUUID);
+var light = exports.accessory = new Accessory('LightXYZ', lightUUID);
 
 // Add properties for publishing (in case we're using Core.js and not BridgedCore.js)
 light.username = "1A:2B:3C:4D:5E:FF";
--- accessories/NewLight_accessory.js
+++ accessories/NewLight_accessory.js
@@ -52,7 +52,7 @@ var NEW_LIGHT = {
     identify: function () {
         console.log("Identify the light!");
     }
-}
+};
 
 // Generate a consistent UUID for our light Accessory that will remain the same even when
 // restarting our server. We use the `uuid.generate` helper function to create a deterministic
@@ -60,10 +60,10 @@ var NEW_LIGHT = {
 var lightUUID = uuid.generate('hap-nodejs:accessories:NewLight');
 
 // This is the Accessory that we'll return to HAP-NodeJS that represents our fake light.
-var light = exports.accessory = new Accessory('Light', lightUUID);
+var light = exports.accessory = new Accessory('NewLight', lightUUID);
 
 // Add properties for publishing (in case we're using Core.js and not BridgedCore.js)
-light.username = "1A:2B:3C:4D:5E:FF";
+light.username = "1A:2B:3C:4D:5E:AF";
 light.pincode = "031-45-154";
 
 // set some basic properties (these values are arbitrary and setting them is optional)
@@ -71,7 +71,7 @@ light
     .getService(Service.AccessoryInformation)
     .setCharacteristic(Characteristic.Manufacturer, "Oltica")
     .setCharacteristic(Characteristic.Model, "Rev-1")
-    .setCharacteristic(Characteristic.SerialNumber, "A1S2NASF88EW");
+    .setCharacteristic(Characteristic.SerialNumber, "000000000001");
 
 // listen for the "identify" event for this Accessory
 light.on('identify', function (paired, callback) {
